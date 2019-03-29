e14a648423c2e9a92d631246c08069f4cc2352a8
jncka <45356600+jncka@users.noreply.github.com>
2019-03-29 16:55:07 -0700

    lo que hicimos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Esto es una practica eh.");
+	System.out.print("Esto es una practica PT.2");
     }
 }
